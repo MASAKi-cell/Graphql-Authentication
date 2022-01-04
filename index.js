ea963bd042d4388248fbb1bf562c0556da31807e
masaki.takahashi <masaki.takahashi@milab-inc.com>
2022-01-04 09:32:22 +0900

    add graphql file

---
+++ index.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require('apollo-server-express')
+const exporess = require(`express`)
+const exporessPlayground = require(`graphql-playground-middleware-express`).default
+const { readFileSync } = require(`fs`)
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+const resolvers = require(`./resolvers/resolvers`)
+const { MongoClient } = require(`mongodb`)
+
+require('dotenv').config()
+
+async function start() {
+  let app = exporess()
+  const MONGO_DB = process.env.DB_HOST
+
+  const client = await MongoClient.content(
+    MONGO_DB,{ userNewUrlParser: true }
+  )
+
+  const db = client.db()
+  const context = { db }
+
+  const server = new ApolloServer({ typeDefs, resolvers })
+  server.applyMiddleware({ app })
+
+  app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
+  app.get(`/playground`, exporessPlayground({ endpoint: `/graphql` }))
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`GraphQL Server runing @http://localhost:4000${server.graphqlPath}`)
+  )
+}
+
+/*
+start関数を実行 → データベースに接続する(非同期)。
+データベースの接続が成功するまで待機し、成功したらその接続を、コンテキストオブジェクトに追加してサーバーを起動する。
+*/
+start()
---
+++ resolvers/resolvers.js
@@ -0,0 +1,50 @@
+const { GraphQLScalarType } = require('graphQL')
+
+let _id = 0;
+let photos = [];
+
+// 写真をデータベースに追加する場合、ユーザーがログインしている必要がある。
+const resolvers = {
+    Query: {
+      totaPhotos: (parent, args, { db }) => db.collection('photos').estimatedDocumentCount(),
+      allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
+      totalUsers: (parent, args, { db }) => db.collection('users').estimatedDocumentCount(),
+      allUsers: (parent, args, { db }) => db.collection('users').find().toArray()
+    },
+    Mutation: {
+      postPhoto(parent, args) {
+
+        let newPhoto = {
+          id: _id++,
+          ...args.input,
+          created: new Date()
+        }
+        photos.push(args)
+        return newPhoto;
+      }
+    },
+    Photo: {
+      url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+      postedBy: parent => {
+        return photos.find(u => u.githubLogin === parent.githubLogin);
+    },
+    taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
+    .map(tag => tag.userID)
+    .map(userID => taggedUsers.find(u => u.githubLogin === userID))
+},
+User: {
+  postedPhotos: parent => {
+    return photos.filter(p => p.githubLogin === parent.githubLogin);
+  },
+  inPhotos: parent => tags.filter(tag => tag.UserID === parent.id)
+  .map(tag => tag.photoID)
+  .map(photoID => postedPhotos.find(p => p.id === photoID))
+  },
+  DateTime: new GraphQLScalarType({
+    name: `DateTime`,
+    description: `A Valid date time Value`,
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral : ast => ast.value,
+  })
+}
