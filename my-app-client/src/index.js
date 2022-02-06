a7b56d3cab107b2eb04e8060229d22fcbbeb61a8
masaki.takahashi <bje35149@gmail.com>
2022-02-06 14:20:27 +0900

    一部修正

--- my-app-client/src/index.js
+++ my-app-client/src/index.js
@@ -1,4 +1,4 @@
-import ApolloClient, { gql } from 'apollo-bost';
+import ApolloClient, { gql } from 'apollo-boost';
 
 const clinet = new ApolloClient({ url: 'http://localhost:4000/graphql' })
 
@@ -8,7 +8,7 @@ const query = gql `
     totalPhotos
   }
 `
-// クエリ文をconsoleに出力、エラーが発生した場合も同様
+// クエリ文をconsoleに出力、エラーが発生した場合も同様にconsoleに出力する
 clinet.query({ query })
    .then(({ data }) => console.log(data))
    .catch(console.error)
